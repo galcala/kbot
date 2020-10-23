6ebd8e669918825f81f795a864cf6cd65fdca1f2
Giancarlo Alcala <giancarlo.alcala.m@gmail.com>
2020-10-23 00:28:24 +0000

    Impresion linea Cantidad * PU = Total

--- factura.rb
+++ factura.rb
@@ -8,6 +8,8 @@ pu= ARGV[1]
 n_cantidad=cantidad.to_i
 n_pu=pu.to_i
 
-n_total=multiplicar(n_cantidad,n_pu)
+n_subtotal=multiplicar(n_cantidad,n_pu)
+n_total=n_subtotal
 
+puts "# #{n_cantidad} * $#{n_pu} = $#{n_subtotal}"
 puts "Total = $#{n_total}"
