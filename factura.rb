5c1df3d937c57589567f6f9fdfc2d23f37d128cd
Giancarlo Alcala <giancarlo.alcala.m@gmail.com>
2020-10-23 00:16:11 +0000

    Funcion multiplicacion Cantidad y PU

--- factura.rb
+++ factura.rb
@@ -1,6 +1,17 @@
+def multiplicar(n_cantidad,n_pu)
+    @n_total = n_cantidad * n_pu
+end
+
 cantidad= ARGV[0]
 pu= ARGV[1]
+
+n_cantidad=cantidad.to_i
+n_pu=pu.to_i
+
+n_total=0
+
 
 puts "hola, mundo"
 puts "#{cantidad}"
 puts "#{pu}"
+puts multiplicar(n_cantidad,n_pu)
