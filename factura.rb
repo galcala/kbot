86de8d19d3adc7515a820d90cb2f0c6f5e5994c2
Giancarlo Alcala <giancarlo.alcala.m@gmail.com>
2020-10-23 00:35:24 +0000

    Funcion calculo Impuesto Fijo

--- factura.rb
+++ factura.rb
@@ -2,14 +2,26 @@ def multiplicar(n_cantidad,n_pu)
     n_cantidad * n_pu
 end
 
+def aplicar_impuesto(n_subtotal,estado)
+    case estado
+    when "CA"
+        n_subtotal*0.1
+    else
+        n_subtotal*0.1
+    end
+end
+
 cantidad= ARGV[0]
 pu= ARGV[1]
+estado= 'CA'
 
 n_cantidad=cantidad.to_i
 n_pu=pu.to_i
 
 n_subtotal=multiplicar(n_cantidad,n_pu)
+n_impuesto=aplicar_impuesto(n_subtotal,estado)
 n_total=n_subtotal
 
 puts "# #{n_cantidad} * $#{n_pu} = $#{n_subtotal}"
+puts n_impuesto
 puts "Total = $#{n_total}"
